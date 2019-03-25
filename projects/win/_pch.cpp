8005cd6c63245b73ba0905906b25c3ca97a0cc98
Z <z@mysoft.tech>
2019-03-25 13:39:24 +0800

    Initialize project.

---
+++ projects/win/_pch.cpp
@@ -0,0 +1,10 @@
+// -------------------------------------------------
+// ZedKit
+// -------------------------------------------------
+//   File Name: _pch.cpp
+// Description: Source for Pre-compiled Header
+//      Author: Ziming Li
+//     Created: 2019-03-25
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
---
+++ src/_pc.h
@@ -0,0 +1,19 @@
+// -------------------------------------------------
+// ZedKit
+// -------------------------------------------------
+//   File Name: _pc.h
+// Description: Pre-compiled Header File
+//      Author: Ziming Li
+//     Created: 2019-03-25
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef ZEDKIT__PC_H
+#define ZEDKIT__PC_H
+
+#pragma once
+
+#include <cassert>
+
+#endif // ZEDKIT__PC_H
