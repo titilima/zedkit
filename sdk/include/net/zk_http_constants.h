8a457f62fa3304713c446214e75903053a80787d
Z <z@mysoft.tech>
2019-03-25 18:14:54 +0800

    Add HTTP header constant strings.

---
+++ sdk/include/net/zk_http_constants.h
@@ -0,0 +1,26 @@
+// -------------------------------------------------
+// ZedKit
+// -------------------------------------------------
+//   File Name: zk_http_constants.h
+// Description: HTTP Constants
+//      Author: Ziming Li
+//     Created: 2019-03-25
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#ifndef ZEDKIT_HTTP_CONSTANTS_H
+#define ZEDKIT_HTTP_CONSTANTS_H
+
+#pragma once
+
+namespace ZedKit {
+
+namespace HTTPHeaders {
+extern const char AcceptEncoding[];
+extern const char AcceptLanguage[];
+} // HTTPHeaders
+
+} // namespace ZedKit
+
+#endif // ZEDKIT_HTTP_CONSTANTS_H
---
+++ src/net/http_constants.cpp
@@ -0,0 +1,21 @@
+// -------------------------------------------------
+// ZedKit
+// -------------------------------------------------
+//   File Name: http_constants.cpp
+// Description: HTTP Constants
+//      Author: Ziming Li
+//     Created: 2019-03-25
+// -------------------------------------------------
+// Copyright (C) 2019 MingYang Software Technology.
+// -------------------------------------------------
+
+#include "net/zk_http_constants.h"
+
+namespace ZedKit {
+
+namespace HTTPHeaders {
+const char AcceptEncoding[] = "Accept-Encoding";
+const char AcceptLanguage[] = "Accept-Language";
+} // HTTPHeaders
+
+} // namespace ZedKit
